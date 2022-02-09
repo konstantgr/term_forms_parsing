745435af6684fd57ddf174b0d54dd8f1d95bb733
konstantgr <60608301+konstantgr@users.noreply.github.com>
2022-02-09 23:17:26 +0300

    change button positions

--- app.py
+++ app.py
@@ -10,30 +10,36 @@ def run():
         df = pd.read_csv(uploaded_file)
         st.write(df)
 
-        if st.button('Professors'):
-            get_people_data(df)
-            shutil.make_archive('myfile', 'zip', 'people')
+        col1, col2, col3 = st.columns([1, 1, 1])
 
-            with open("myfile.zip", "rb") as fp:
-                btn = st.download_button(
-                    label="Download ZIP",
-                    data=fp,
-                    file_name="professors.zip",
-                    mime="application/zip"
-                )
+        with col1:
+            if st.button('Professors'):
+                get_people_data(df)
+                shutil.make_archive('myfile', 'zip', 'people')
 
-        elif st.button('Classes'):
-            get_subjects_data(df)
-            shutil.make_archive('myfile', 'zip', 'subjects')
+                with open("myfile.zip", "rb") as fp:
+                    with col3:
+                        btn = st.download_button(
+                            label="Download ZIP",
+                            data=fp,
+                            file_name="professors.zip",
+                            mime="application/zip"
+                        )
 
-            with open("myfile.zip", "rb") as fp:
-                btn = st.download_button(
-                    label="Download ZIP",
-                    data=fp,
-                    file_name="subjects.zip",
-                    mime="application/zip"
-                )
+        with col2:
+            if st.button('Classes'):
+                get_subjects_data(df)
+                shutil.make_archive('myfile', 'zip', 'subjects')
 
+                with col3:
+                    with open("myfile.zip", "rb") as fp:
+                        btn = st.download_button(
+                            label="Download ZIP",
+                            data=fp,
+                            file_name="subjects.zip",
+                            mime="application/zip"
+                        )
+                        
 
 if __name__ == '__main__':
     run()
